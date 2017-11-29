04832ad10bc4a75cb390925e736b8f3e4742755d
Mano <manokum36@gmail.com>
2017-11-29 07:22:44 +0530

    AL-1234: Inclluding api call for getting the list of dogs

--- apps.js
+++ apps.js
@@ -2,7 +2,11 @@ var app = angular.module("app",[]);
 
 app.controller("MainController",function($scope,$http){
     $http.get("https://dog.ceo/api/breeds/list/all").then(function(res){
-        $scope.test = res.data;
+        $scope.test = res.data.message;
+        $scope.test1 =[];
+        for (var key in $scope.test) {
+         $scope.test1.push(key);
+}
     })
 
 });
